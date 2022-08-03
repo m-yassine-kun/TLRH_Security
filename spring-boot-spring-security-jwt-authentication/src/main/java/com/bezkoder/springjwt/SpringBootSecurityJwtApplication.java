07db5074204e9872c5399ca1e70c0418bae0e5ea
medYassineHR <harrachmin.m.yassine@gmail.com>
2022-08-03 13:18:16 +0100

    first commit

---
+++ spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,18 @@
+package com.bezkoder.springjwt;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
